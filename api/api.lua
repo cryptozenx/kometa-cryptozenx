69db3e72e1d4cb3f3aa56e39818fb4fa915e0a2a
cryptozenx <114301840+cryptozenx@users.noreply.github.com>
2023-01-04 19:47:20 +0500

    Add files via upload

--- api/api.lua
+++ api/api.lua
@@ -1,150 +1,156 @@
-local API = {}
-
-function API:Player() -- returns the player
-    return game:GetService('Players').LocalPlayer
-end
-
-function API:Character() -- returns the player character
-    return self:Player().Character
-end
-
-function API:Root() -- returns the player humanoid root
-    return self:Character():WaitForChild("HumanoidRootPart")
-end
-
-function API:Humanoid() -- returns the humanoid
-    return self:Character():WaitForChild("Humanoid")
-end
-
-function API:Tween(Time, CF) -- tween to position
-    game:GetService('TweenService'):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(Time, Enum.EasingStyle.Linear), {CFrame = CF}):Play() 
-    task.wait(Time)
-end
-
-function API:WalkTo(Position) -- walk to position
-    self:Humanoid():MoveTo(Position)
-end
-
-function API:Teleport(CFrame) -- teleport to position
-    self:Root().CFrame = CFrame
-end
-
-function API:Magnitude(Pos1, Pos2) -- return positions magnitude
-    return (Pos1 - Pos2).Magnitude
-end
-
-function API:Pathfind(Position)
-    local PathfindingService = game:GetService("PathfindingService")
-    local Humanoid = self:Humanoid()
-    local Root = self:Root()
-    local Path = PathfindingService:CreatePath({
-        AgentCanJump = true,
-        WaypointSpacing = 1
-    })
-    Path:ComputeAsync(Root.Position, Position)
-    local waypoints = Path:GetWaypoints()
-    for i, waypoint in ipairs(waypoints) do
-        Humanoid:MoveTo(waypoint.Position)
-        Humanoid.MoveToFinished:Wait()
-        if waypoint.Action == Enum.PathWaypointAction.Jump then
-            Humanoid.Jump = true
-        end
+local API = {} do
+    
+    function API:Player() -- returns the player
+        return game:GetService('Players').LocalPlayer
     end
-end
+    
+    function API:Character() -- returns the player character
+        return self:Player().Character
+    end
+    
+    function API:Root() -- returns the player humanoid root
+        return self:Character():WaitForChild("HumanoidRootPart")
+    end
+    
+    function API:Humanoid() -- returns the humanoid
+        return self:Character():WaitForChild("Humanoid")
+    end
+    
+    function API:Tween(Time, CF) -- tween to position
+        game:GetService('TweenService'):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(Time, Enum.EasingStyle.Linear), {CFrame = CF}):Play() 
+        task.wait(Time)
+    end
 
-function API:Notify(Title, Description, Duration) -- send notification
-    pcall(function()
-        game:GetService('StarterGui'):SetCore("SendNotification", {
-            Title = Title;
-            Text = Description;
-            Duration = Duration;
+    function API:TweenNoDelay(Time, CF) -- tween to position without delay
+        game:GetService('TweenService'):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(Time, Enum.EasingStyle.Linear), {CFrame = CF}):Play() 
+    end
+    
+    function API:WalkTo(Position) -- walk to position
+        self:Humanoid():MoveTo(Position)
+    end
+    
+    function API:Teleport(CFrame) -- teleport to position
+        self:Root().CFrame = CFrame
+    end
+    
+    function API:Magnitude(Pos1, Pos2) -- return positions magnitude
+        return (Pos1 - Pos2).Magnitude
+    end
+    
+    function API:Pathfind(Position)
+        local PathfindingService = game:GetService("PathfindingService")
+        local Humanoid = self:Humanoid()
+        local Root = self:Root()
+        local Path = PathfindingService:CreatePath({
+            AgentCanJump = true,
+            WaypointSpacing = 1
         })
-    end)
-end
-
-function API:ToHMS(Time) -- return time string
-    local Minutes = (Time - Time % 60) / 60
-    Time -= Minutes * 60
-    local Hours = (Minutes - Minutes % 60) / 60
-    Minutes -= Hours * 60
-    return string.format("%02i", Hours)..":"..string.format("%02i", Minutes)..":"..string.format("%02i", Time)
-end
-
-function API:SuffixString(String) -- return suffix string
-    local Suffixes = {"k", "m", "b", "t", "q", "Q", "sx", "sp", "o", "n", "d"}
-    for Index = #Suffixes, 1, -1 do
-        local Pow = math.pow(10, Index * 3)
-        if String >= Pow then
-            return ("%.1f"):format(String / Pow) .. Suffixes[Index]
-        end
-    end
-    return tostring(String)
-end
-
-function API:TableFind(Table, Value) -- return index
-    for index, value in next, Table do
-        if value == Value then
-            return index
-        end
-    end
-end
-
-function API:FindValue(Table, Value) -- return boolean
-    if type(Table) == "table" then
-        for _, value in next, Table do
-            if value == Value then
-                return true
+        Path:ComputeAsync(Root.Position, Position)
+        local waypoints = Path:GetWaypoints()
+        for i, waypoint in ipairs(waypoints) do
+            Humanoid:MoveTo(waypoint.Position)
+            Humanoid.MoveToFinished:Wait()
+            if waypoint.Action == Enum.PathWaypointAction.Jump then
+                Humanoid.Jump = true
             end
         end
-    else
+    end
+    
+    function API:Notify(Title, Description, Duration) -- send notification
+        pcall(function()
+            game:GetService('StarterGui'):SetCore("SendNotification", {
+                Title = Title;
+                Text = Description;
+                Duration = Duration;
+            })
+        end)
+    end
+    
+    function API:ToHMS(Time) -- return time string
+        local Minutes = (Time - Time % 60) / 60
+        Time -= Minutes * 60
+        local Hours = (Minutes - Minutes % 60) / 60
+        Minutes -= Hours * 60
+        return string.format("%02i", Hours)..":"..string.format("%02i", Minutes)..":"..string.format("%02i", Time)
+    end
+    
+    function API:SuffixString(String) -- return suffix string
+        local Suffixes = {"k", "m", "b", "t", "q", "Q", "sx", "sp", "o", "n", "d"}
+        for Index = #Suffixes, 1, -1 do
+            local Pow = math.pow(10, Index * 3)
+            if String >= Pow then
+                return ("%.1f"):format(String / Pow) .. Suffixes[Index]
+            end
+        end
+        return tostring(String)
+    end
+    
+    function API:TableFind(Table, Value) -- return index
+        for index, value in next, Table do
+            if value == Value then
+                return index
+            end
+        end
+    end
+    
+    function API:FindValue(Table, Value) -- return boolean
+        if type(Table) == "table" then
+            for _, value in next, Table do
+                if value == Value then
+                    return true
+                end
+            end
+        else
+            return false
+        end
         return false
     end
-    return false
-end
-
-function API:GetPartWithName(Name, Path) -- return part
-    for _, Object in next, Path:GetChildren() do
-        if (Object.Name:match(Name)) then
-            return Object
-        end
-    end
-end
-
-function API:GetBiggestModel(Path) -- return biggest part
-    local Part
-    for _, Object in next, Path:GetChildren() do
-        if Object:IsA("Model") then
-            if Part == nil then
-                Part = Object
-            end
-            if Object:GetExtentsSize().Y > Part:GetExtentsSize().Y then
-                Part = Object
+    
+    function API:GetPartWithName(Name, Path) -- return part
+        for _, Object in next, Path:GetChildren() do
+            if (Object.Name:match(Name)) then
+                return Object
             end
         end
     end
-    return Part
-end
-
-function API:ImageHook(Hook, Description, Title, Image) -- send webhook
-    pcall(function()
-        local Embed = {
-            color = '3454955';
-            title =  Title;
-            description = Description;
-            thumbnail = {
-                url = Image;
+    
+    function API:GetBiggestModel(Path) -- return biggest part
+        local Part
+        for _, Object in next, Path:GetChildren() do
+            if Object:IsA("Model") then
+                if Part == nil then
+                    Part = Object
+                end
+                if Object:GetExtentsSize().Y > Part:GetExtentsSize().Y then
+                    Part = Object
+                end
+            end
+        end
+        return Part
+    end
+    
+    function API:ImageHook(Hook, Description, Title, Image) -- send webhook
+        pcall(function()
+            local Embed = {
+                color = '3454955';
+                title =  Title;
+                description = Description;
+                thumbnail = {
+                    url = Image;
+                };
             };
-        };
-
-        (syn and syn.request or http_request) {
-            Url = Hook;
-            Method = 'POST';
-            Headers = {
-                ['Content-Type'] = 'application/json';
+    
+            (syn and syn.request or http_request) {
+                Url = Hook;
+                Method = 'POST';
+                Headers = {
+                    ['Content-Type'] = 'application/json';
+                };
+                Body = game:GetService('HttpService'):JSONEncode( { content = Content; embeds = { Embed } } );
             };
-            Body = game:GetService('HttpService'):JSONEncode( { content = Content; embeds = { Embed } } );
-        };
-    end)
-end
+        end)
+    end
 
+end
+------ 
 return API
